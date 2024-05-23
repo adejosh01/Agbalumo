17c9e93894f116f39465a1b5714545d48c3ad787
adejosh01 <84485276+adejosh01@users.noreply.github.com>
2024-05-23 11:51:44 +0100

    test

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className='header'>
         <div className="header-contents">
-            <h2>Order for your favourite food here</h2>
+            <h2>Order for your favourite food </h2>
             <p>
                 Choose from a diverse menu featuring a delectable array of dishes crafted and elevate
                 your dining experience, one delicious meal at a time.
